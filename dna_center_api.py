4609849f6ba899e6df04596908d6f188ec324726
Dustin <dustin.naquin@gmail.com>
2023-09-27 21:34:33 -0500

    changed the dna center code to make it pretty.

--- dna_center_api.py
+++ dna_center_api.py
@@ -1,20 +1,38 @@
 from dnacentersdk import DNACenterAPI as API
 from pprint import pprint
 import datetime
-import calendar
 
-ep = str(int(datetime.datetime(2023, 8, 22, 20, 41, 29, 0, datetime.UTC).timestamp())*1000)
 
-DNAC = API(username="devnetuser", password="Cisco123!", base_url="https://sandboxdnac.cisco.com", verify=False)
+DT_OBJ = datetime.datetime(2023, 8, 22, 20, 41, 29, 0, datetime.UTC)
+EP = str(int(DT_OBJ.timestamp())*1000)
+
+DNAC = API(username="devnetuser", 
+           password="Cisco123!", 
+           base_url="https://sandboxdnac.cisco.com", 
+           verify=False)
 
 DEVICES = DNAC.devices.get_device_list()
-CLIENTS = DNAC.clients.get_overall_client_health(timestamp=ep)
+CLIENTS = DNAC.clients.get_overall_client_health(timestamp=EP)
+
+print(f'{"Hostname":33s} \
+        {"|":1}{"Device Type":53s} \
+        {"|":1}{"Uptime":15s}')
+
 
 for DEVICE in DEVICES.response:
-    pprint(f"Hostname:  {DEVICE.hostName}, Type: {DEVICE.type}, Uptime: {DEVICE.upTime}")
+    print(f'{DEVICE.hostname:28s} \
+             {"|":1}{DEVICE.type:48s} \
+             {"|":1}{DEVICE.upTime:15s}')
+
+print('\n')
+
+print(f'{"Client Category":33s} \
+        {"|":1}{"Number of Clients":53s} \
+        {"|":1}{"Clients Score":15s}')
 
 
-print('{0:25s}{1:1}{2:45s}{3:1}{4:15s}'.format("Client Category", "|", "Number of Clients", "|", "Clients Score"))
 for CLIENT in CLIENTS.response:
     for score in CLIENT.scoreDetail:
-        print('{0:25s}{1:1}{2:<45d}{3:1}{4:<15d}'.format(score.scoreCategory.value, "|", score.clientCount, "|", score.scoreValue))
+        print(f'{score.scoreCategory.value:25s} \
+                {"|":1}{score.clientCount:<45d} \
+                {"|":1}{score.scoreValue:<15d}')
